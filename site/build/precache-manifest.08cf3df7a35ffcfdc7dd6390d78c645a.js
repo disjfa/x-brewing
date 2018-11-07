18e0de0b4339ce7f31b0007f4fc8db5d0a49ec7b
disjfa <disjfa@disjfa.nl>
2018-11-07 21:58:57 +0000

    Added colors

---
+++ site/build/precache-manifest.08cf3df7a35ffcfdc7dd6390d78c645a.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "dff9f1e0a67b87566ccb",
+    "url": "/build/theme.js"
+  },
+  {
+    "revision": "67beefbf39ac2f274744",
+    "url": "/build/app.js"
+  }
+];
