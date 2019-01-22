dd451e45d254411a9b0832ea13d43e444ec34c74
disjfa <disjfa@disjfa.nl>
2019-01-22 21:47:39 +0000

    Added instagram

---
+++ site/build/precache-manifest.bc0834e1905248d67e908bff6e57623a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "2273e3d8ad9264b7daa5bdbf8e6b47f8",
+    "url": "/build/images/marker-icon.2273e3d8.png"
+  },
+  {
+    "revision": "401d815dc206b8dc1b17cd0e37695975",
+    "url": "/build/images/marker-icon-2x.401d815d.png"
+  },
+  {
+    "revision": "171dfa23615fe11133fe",
+    "url": "/build/runtime.js"
+  },
+  {
+    "revision": "44a526eed258222515aa21eaffd14a96",
+    "url": "/build/images/marker-shadow.44a526ee.png"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/build/fonts/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/build/images/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "a6137456ed160d7606981aa57c559898",
+    "url": "/build/images/layers.a6137456.png"
+  },
+  {
+    "revision": "4f0283c6ce28e888000e978e537a6a56",
+    "url": "/build/images/layers-2x.4f0283c6.png"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/build/images/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/build/fonts/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/build/fonts/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/build/fonts/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/build/fonts/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/build/fonts/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/build/fonts/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/build/fonts/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "03740158880dfb240299",
+    "url": "/build/app.js"
+  },
+  {
+    "revision": "03740158880dfb240299",
+    "url": "/build/app.css"
+  }
+];
